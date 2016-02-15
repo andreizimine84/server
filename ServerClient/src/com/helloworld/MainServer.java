9f19a8cb9b1d0826de7f11a78ae54260abfb064f
andrei <andreizimine84@gmail.com>
2016-02-15 13:28:10 +0100

    Server-side - updates with JNI

--- ServerClient/src/com/helloworld/MainServer.java
+++ ServerClient/src/com/helloworld/MainServer.java
@@ -1,18 +1,24 @@
-
 package com.helloworld;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-
 import java.io.ByteArrayOutputStream;
-
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.EmptyStackException;
-import org.eclipse.jetty.server.Server;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.io.EofException;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class MainServer extends AbstractHandler
@@ -20,56 +26,80 @@ public class MainServer extends AbstractHandler
 	FileOutputStream fos = null;
 	ByteArrayOutputStream baos = null;
 	InputStream is = null;
+    private static final Logger LOG = Logger.getLogger(MainServer.class.getName());
 	
 	int bytesRead = -1;
-	int bytesAvailable = -1;
-	byte[] buffer = null;
-
+	//byte[] buffer = null;
+	MessageDigest md = null;
 	public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
                        HttpServletResponse response) 
-        throws IOException, ServletException
+        throws IOException, ServletException, EofException
     {
-
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
         response.getWriter().println("<h1>Andrei Zimine</h1>");
-        
         is = request.getInputStream();
-        bytesAvailable = is.available();
-        System.out.println("hello" + bytesAvailable);
-        
+        int contentLength = request.getContentLength();
+        String sha1FromSender = request.getHeader("X-checksum");
         try
         {
-            buffer = new byte[bytesAvailable];
-            
+        	byte[] buffer = new byte[contentLength];
             baos = new ByteArrayOutputStream();
-            
-            bytesRead = is.read(buffer);
-        	System.out.println("bytesRead" + bytesRead);
-        	if(bytesRead != -1 && bytesAvailable != 0)
-        		fos = new FileOutputStream("output_" + request.hashCode() + ".txt");       	
-        	else if (bytesAvailable == 0)
-            	bytesRead = -1;
-        	else
-            	throw new EmptyStackException();
+        	bytesRead = is.read(buffer, 0, is.available());
+            while(bytesRead != -1)
+        		bytesRead = is.read(buffer, bytesRead, is.available());
+
+    		StringBuffer sb = new StringBuffer();
+    		//byte[] mdbytes = null;
+       		if(buffer.length == contentLength){
+            	md = MessageDigest.getInstance("SHA-1");
+    			md.update(buffer);
+    			byte[] mdbytes = md.digest();
+    	                
+    	        for (int i = 0; i < mdbytes.length; i++) {
+    	          sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+    	        }
+
+    	        md.reset();
+    	        mdbytes = new byte[0];
+    		}
+       		
+       		System.out.println("sha1 " + sha1FromSender + "--" + sb.toString());
+       		/*if(!sha1FromSender.equals(sb.toString())){
+       	        LOG.log(Level.WARNING,"checksum does not match!");
+       	        LOG.warning("checksum does not match!");
+       	        LOG.severe("Exiting!");
+       	        response.setStatus(HttpServletResponse.SC_CONFLICT);
+       		}
+       		else{*/
+       			fos = new FileOutputStream("output_temp" + request.hashCode() + ".txt");       	
+
+            	baos.write(buffer);
+       			baos.writeTo(fos);
+       		//}
+       		sb = null;
+       		//mdbytes = null;
+        	baos.flush();
+        	baos.close();
+        	baos.reset();
+        	is.close();
+        	fos.flush();
+        	fos.close();
+        	buffer = new byte[0];
+        	contentLength = 0;
         	
-        	while (bytesRead > -1) 
-        	{
-        		System.out.println(bytesRead);
-        		baos.write(buffer);
-        		baos.writeTo(fos);
-        		bytesRead = is.read(buffer);
-        	}
+        	//buffer = null;
         }
         
     	catch(IOException e)
     	{
     		System.err.println("Caught IOException: " + e.getMessage());
-    	}
-
+    	} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
         finally 
         {
         	if(fos != null)
@@ -82,6 +112,6 @@ public class MainServer extends AbstractHandler
         Server server = new Server(8080);
         server.setHandler(new MainServer());
         server.start();
-        server.join();        
+        server.join(); 
     }
 }
